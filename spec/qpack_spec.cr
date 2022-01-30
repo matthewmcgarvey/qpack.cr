b0d56f8a8012c8b107a37518b4dea666d4c86be3
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-01-29 21:07:23 -0600

    lib init

---
+++ spec/qpack_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe QPack do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/qpack.cr
@@ -0,0 +1,5 @@
+module QPack
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
